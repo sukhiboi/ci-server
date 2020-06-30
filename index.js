62cbce5978036cd17da394f62217de9c9f3f18cf
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-07-01 00:31:43 +0530

    updated linter

--- index.js
+++ index.js
@@ -1,58 +1,46 @@
-const { spawn } = require('child_process');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+const Listr = require('listr');
+const {
+  cloneRepo,
+  installEslint,
+  checkEslintrc,
+  lint,
+  deleteLocalRepo,
+} = require('./task');
 const { log } = console;
-const rightExitCode = 0;
 
-const lintRepo = function (cloneURL, name) {
-  log(`Cloning repo ${cloneURL}`);
-  const child = spawn('git', ['clone', cloneURL]);
-  return new Promise((res) => {
-    child.on('exit', (code) => {
-      if (code === rightExitCode) {
-        log('installing eslint');
-        const installEslint = spawn('npm', ['install', 'eslint']);
-        const lintProcess = new Promise((res) => {
-          installEslint.on('exit', (code) => {
-            if (code === rightExitCode) {
-              log('linting');
-              const do_ = spawn('eslint', ['*.js']);
-              do_.stdout.setEncoding('utf-8');
-              do_.stdout.on('data', (data) => {
-                log(data);
-                res(
-                  new Promise((res) => {
-                    const deleteChild = spawn('rm', ['-rf', name]);
-                    log('deleting repo');
-                    deleteChild.on('exit', res);
-                  })
-                );
-              });
-              do_.on('exit', log);
-            }
-          });
-        });
-        res(lintProcess);
-      }
-    });
-  });
+const lintRepo = function (req, response) {
+  const cloneURL = req.body.repository.clone_url;
+  const repoName = req.body.repository.name;
+  const tasks = new Listr([
+    {
+      title: 'cloning repo',
+      task: () => cloneRepo(cloneURL),
+    },
+    {
+      title: 'installing eslint',
+      task: () => installEslint(repoName),
+    },
+    {
+      title: 'checking .eslintrc',
+      task: () => checkEslintrc(repoName),
+    },
+    {
+      title: 'linting',
+      task: () => lint(repoName, response),
+    },
+    {
+      title: 'deleting local repo',
+      task: () => deleteLocalRepo(repoName),
+    },
+  ]);
+  tasks.run();
 };
 
 app.use(bodyParser.json());
-app.post('/github', (req, res) => {
-  if (req.body.repository.clone_url) {
-    lintRepo(req.body.repository.clone_url, req.body.repository.name).then(
-      (data) => {
-        log(data, 'done');
-      }
-    );
-  } else {
-    res.send('invalid request');
-  }
-  res.end();
-});
+app.post('/github', lintRepo);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => log(`listening to ${PORT}`));
---
+++ task.js
@@ -0,0 +1,63 @@
+const { readFileSync, existsSync, writeFileSync } = require('fs');
+const { exec } = require('child_process');
+
+const cloneRepo = function (cloneURL) {
+  return new Promise((res) => {
+    exec(`git clone ${cloneURL}`, (err, stdout) => {
+      if (err) {
+        throw new Error(err);
+      }
+      res(stdout);
+    });
+  });
+};
+
+const installEslint = function (repoName) {
+  return new Promise((res) => {
+    exec(`cd ${repoName}; npm init -y; npm install eslint`, (err, stdout) => {
+      if (err) {
+        throw new Error(err);
+      }
+      res(stdout);
+    });
+  });
+};
+
+const checkEslintrc = function (repoName) {
+  return new Promise((res) => {
+    const isLintFileAvailable = existsSync(`./${repoName}/.eslintrc`);
+    if (!isLintFileAvailable) {
+      writeFileSync(`./${repoName}/.eslintrc`, readFileSync('./.eslintrc'));
+    }
+    res();
+  });
+};
+
+const lint = function (repoName, response) {
+  return new Promise((res) => {
+    exec(`cd ${repoName}; eslint *.js`, (err, stdout, stderr) => {
+      if (err) {
+        response.end(JSON.stringify({ errors: stderr, warnings: stdout }));
+      } else {
+        response.end(JSON.stringify({ warnings: stdout }));
+      }
+      res();
+    });
+  });
+};
+
+const deleteLocalRepo = function (repoName) {
+  return new Promise((res) => {
+    if (repoName) {
+      exec(`rm -rf ${repoName}`, res);
+    }
+  });
+};
+
+module.exports = {
+  cloneRepo,
+  installEslint,
+  checkEslintrc,
+  lint,
+  deleteLocalRepo,
+};
