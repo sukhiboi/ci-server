6d6b2b976a065dd04c2918c086bd0bfe34ca5344
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-06-30 21:20:41 +0530

    updated port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 
-const PORT = 4000 || process.env.PORT;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`listening to ${PORT}`));
