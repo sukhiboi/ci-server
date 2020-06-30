e4cbc23e78f4233e80afd39a32385a73053c8245
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-06-30 21:58:29 +0530

    add / handler

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.post('/', (req, res) => {
+    const { params, body, query } = res;
+    console.log(params, body, query);
+});
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`listening to ${PORT}`));
