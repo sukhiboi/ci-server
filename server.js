f2e846ff864899dda9fa0dd864a9bdb28c8dfc0c
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-07-01 12:37:27 +0530

    updated server with redis

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const redis = require('redis');
+const client = redis.createClient(process.env.REDIS_URL);
 
 const defaultPort = 4000;
-
 app.use(bodyParser.json());
 
-const jobs = [];
 let id = 0;
 
 const scheduleJob = function (req, res) {
   console.log(`scheduled job ${id}`);
-  jobs.push({ ...req.body, id: id++ });
-  res.send('scheduled');
+  const jobDetails = Object.keys(req.body.repository).reduce((details, key) => {
+    return [...details, key, req.body.repository[key]];
+  }, []);
+  client.hset(`job${id}`, jobDetails);
+  client.lpush('queue', `job${id++}`, () => {
+    res.send('scheduled');
+  });
 };
 
 app.post('/payload', scheduleJob);
-app.get('/job', (req, res) => {
-  if (jobs.length) {
-    res.json(jobs.shift());
-  } else {
-    res.end('no job');
-  }
-});
 
 const PORT = process.env.PORT || defaultPort;
 app.listen(PORT, () => console.log(`listening to ${PORT}`));
